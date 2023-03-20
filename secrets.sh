ebe7dc933fc9605d962e862b7ea3c2f7968d3aea
Carlo Dee <carlodee.official@proton.me>
2023-03-20 15:56:51 +0800

    Update secrets.sh

--- secrets.sh
+++ secrets.sh
@@ -4,3 +4,7 @@ export OPENAI_API="<your-openai-api-key-here>"
 export TELEGRAM_TOKEN="<your-telegram-bot-token-here>"
 
 echo "Done setting up environment variables."
+
+echo "Will now run the bot..."
+
+python3 ./chatgpt_bot.py
